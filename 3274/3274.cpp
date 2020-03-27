7c5d4a73a321d41224a7443edb36c064cf07b885
casc1244 <1356602646@qq.com>
2020-03-27 15:12:18 +0800

    3274.cpp

--- 3274/3274.cpp
+++ 3274/3274.cpp
@@ -2,20 +2,32 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 using namespace std;
+Mat g_sobelGradient_X, g_sobelGradient_Y;
+Mat g_sobelAbsGradient_X, g_sobelAbsGradient_Y;
+int g_sobelKernelSize = 1;
 int main(int argc, const char * argv[]) {
 
 
 	VideoCapture cap(0);
 
 
+
 	while (1)
 	{
 		Mat InMat;
 		Mat OutMat;
+		cv::threshold(InMat, OutMat, 0, 255, THRESH_OTSU);
 		cap >> InMat;
-		Sobel(InMat, OutMat, 3,1,1,1,1);
+			Sobel(InMat, g_sobelGradient_X, CV_16S, 1, 0, (2 * g_sobelKernelSize + 1), 1, 1, BORDER_DEFAULT);
+			convertScaleAbs(g_sobelGradient_X, g_sobelAbsGradient_X);
+
+
+			Sobel(InMat, g_sobelGradient_Y, CV_16S, 0, 1, (2 * g_sobelKernelSize + 1), 1, 1, BORDER_DEFAULT);
+			convertScaleAbs(g_sobelGradient_Y, g_sobelAbsGradient_Y);
+			addWeighted(g_sobelAbsGradient_X, 0.5, g_sobelAbsGradient_Y, 0.5, 0, OutMat);
+			imshow("Sobel边缘检测", OutMat);
 		imshow("InMat", InMat);
-		imshow("OutMat", OutMat);
+		convertScaleAbs(OutMat, OutMat);
 
 		waitKey(30);
 
